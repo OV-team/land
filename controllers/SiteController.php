25fc3debcb370ccc95a6bfd64f369947be9a5bb0
asus4851 <hres.vadym@gmail.com>
2015-11-14 16:10:43 +0200

    fixed admin layout

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -54,6 +54,7 @@ class SiteController extends Controller
 
     public function actionLogin()
     {
+        $this->layout = 'empty';
         if (!\Yii::$app->user->isGuest) {
             return $this->goHome();
         }
--- views/site/login.php
+++ views/site/login.php
@@ -68,3 +68,4 @@ $fieldOptions2 = [
     </div>
     <!-- /.login-box-body -->
 </div><!-- /.login-box -->
+
